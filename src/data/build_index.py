606e4338e23e3f62e51f8a17a834b0f04f9659cf
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2025-07-20 22:47:50 +0200

    Version 1 - Fichier de création des index sur notre jeu de données

---
+++ src/data/build_index.py
@@ -0,0 +1,47 @@
+"""
+build_index.py
+
+Auteurs : BAUDET Quentin & LARMAILLARD-NOIREN Joris
+
+Objectif : Encoder tous les passages JSONL en vecteurs d'embedding via un
+      modèle SentenceTransformer, puis construire et sauvegarder un
+      index FAISS pour recherches de similarité.
+
+Entrée : data/processed/passages.jsonl
+Sortie : models/faiss_index.idx
+Usage :
+    python build_index.py \
+      --passages data/processed/passages.jsonl \
+      --index models/faiss_index.idx
+"""
+
+# ------------------------ Importation des modules ------------------------ #
+from sentence_transformers import SentenceTransformer
+import faiss
+import json
+import numpy as np
+### Chargement des variables d'environnement
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# --------------- Chargement du modèle d'embeddings - BERT ---------------- #
+TRANSFORMER = os.getenv("TRANSFORMER")
+embedder = SentenceTransformer(TRANSFORMER)
+
+texts: list = []
+# ------------------------ Chargement des passages ------------------------ #
+with open("data/processed/passages.jsonl") as f:
+    for line in f:
+        j = json.loads(line)
+        texts.append(j["text"])
+
+# ------------------------- Calcul des embeddings ------------------------- #
+embs = embedder.encode(texts, convert_to_numpy=True, show_progress_bar=True)
+
+# -------------------------- Création de l'index -------------------------- #
+dim = embs.shape[1]
+index = faiss.IndexFlatIP(dim)
+index.add(embs)
+faiss.write_index(index, "models/faiss_index.idx")
